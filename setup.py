3aa44b09f3da8f2eeb57f90680517bbc484ce15c
inflop <inflop@inflop.pl>
2015-01-05 19:06:26 +0100

    Removed unused packages from setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author_email='infloper@gmail.com',
     description='Simple RS232 reader written in Python using pySerial and PyGTK library',
     data_files=[("", glob.glob("main.glade")), os.path.join(gtk_base_path, '..', 'runtime', 'bin', 'gdk-pixbuf-query-loaders.exe'), os.path.join(gtk_base_path, '..', 'runtime', 'bin', 'libxml2-2.dll')],
-    options={'py2exe': {'packages': 'encodings', 'includes': 'ctypes, cairo, pango, pangocairo, atk, gobject, gio, gtk, gtk.glade, glib'}},
+    options={'py2exe': {'packages': 'encodings', 'includes': 'ctypes, cairo, pango, pangocairo, atk, gio'}},
     scripts=["main.py"],
     windows=[{"script": "main.py"}]
 )
